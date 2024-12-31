58cfb1ea5638594c38e1d8c61169a2091678b5c3
david-fisher-1993 <david.1993.fisher@gmail.com>
2024-12-30 21:09:02 -0800

    Removed home manager url release version

--- flake.nix
+++ flake.nix
@@ -2,13 +2,11 @@
     description = "flake for david with Home Manager enabled";
 
     inputs = {
-        # Nixpkgs
         #nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
         # Home manager
-        home-manager.url = "github:nix-community/home-manager/release-24.11";
-        #home-manager.url = "github:nix-community/home-manager";/release-24.11
+        home-manager.url = "github:nix-community/home-manager";
         home-manager.inputs.nixpkgs.follows = "nixpkgs";
     };
 
