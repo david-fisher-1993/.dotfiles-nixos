89a15c37eaa80d60d8e063c074b993218f7caa18
david-fisher-1993 <david.1993.fisher@gmail.com>
2024-12-30 20:34:07 -0800

    home manager url

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
         # Home manager
-        home-manager.url = "github:nix-community/home-manager";
+        home-manager.url = "github:nix-community/home-manager/release-25.05";
         #home-manager.url = "github:nix-community/home-manager";/release-24.11
         home-manager.inputs.nixpkgs.follows = "nixpkgs";
     };
